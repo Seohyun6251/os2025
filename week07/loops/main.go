80d2e4388292d69fb8bd9c84cd66172e8365e144
sinome2006 <sinome2006@gmail.com>
2025-10-13 14:55:01 +0900

    v0.1 bufio & os package

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	r := bufio.NewReader(os.Stdin)
-	i, _ := r.ReadString('\n') // ignore error
+	i, err := r.ReadString('\n') // ignore error
 	fmt.Println(i)
+	// fmt.Println(err)
+	log.Fatal(err) // report error
 }
