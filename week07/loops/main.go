7e24a185885ea687d0ecaf2d2e6af6c6e31604bc
sinome2006 <sinome2006@gmail.com>
2025-10-27 13:41:19 +0900

    update

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -10,24 +10,30 @@ import (
 )
 
 func main() {
+	// shadowing
+	// var int int = 99
+	// var b int = 8
+	// var fmt string = "inha"
+	// fmt.Println(int, b)
+
 	fmt.Print("Enter a score: ")
-	reader := bufio.NewReader(os.Stdin)
-	i, err := reader.ReadString('\n')
+	r := bufio.NewReader(os.Stdin)
+	i, err := r.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	i = strings.TrimSpace(i)
-	score, err := strconv.ParseFloat(i, 64)
+	i = strings.TrimSpace(i)                // 문자열 주위에 붙은 공란 및 탭 키 등 제거
+	score, err := strconv.ParseFloat(i, 64) // 정리된 문자열을 실수타입으로 변환
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	var status string
 	if score >= 60 {
-		status = "passing"
+		status = "Pass"
 	} else {
-		status = "failing"
+		status = "Fail"
 	}
-	fmt.Println("A grade of", score, "is", status)
+	fmt.Println(score, status)
 }
