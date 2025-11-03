9a9a296ca79d1d9a282c789b990c296a9f90d16c
sinome2006 <sinome2006@gmail.com>
2025-11-03 15:26:22 +0900

    week10 package v0.4

---
+++ week10/cmd/ch04-ex04/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"week10/pkg/deutsch"
+	"week10/pkg/greeting"
+	"week10/pkg/korean"
+)
+
+func main() {
+	greeting.Hello()
+	greeting.Hi()
+	deutsch.GutenTag()
+	korean.Anyung()
+	korean.Anyunghasayo()
+
+}
---
+++ week10/pkg/korean/korean.go
@@ -0,0 +1,10 @@
+package korean
+
+import "fmt"
+
+func Anyung() {
+	fmt.Println("안녕")
+}
+func Anyunghasayo() {
+	fmt.Println("안녕하세요")
+}
