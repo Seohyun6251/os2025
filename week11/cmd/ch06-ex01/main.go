086b2a73cbbbaa809a343e4aacd066ccd2db733a
sinome2006 <sinome2006@gmail.com>
2025-11-10 14:34:27 +0900

    week11 slice v0.3

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -3,16 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	// var subjects []string
-	// subjects = make([]string, 3)
 
-	// subjects := make([]string,3)
-	// subjects[0] = "Go"
-	// subjects[2] = "Python"
+	subjects := []string{"Go", "javascript", "Python", "Linux"} // slice literal
 
-	subjects := []string{"Go", "", "Python"} // slice literal
+	subjectsSlice := subjects[1:3] //슬라이싱
+	//l = list()
 
 	for _, subject := range subjects {
 		fmt.Println(subject)
+	}
+	fmt.Println("==================")
+	for i := 0; i < len(subjectsSlice); i++ {
+		fmt.Println(subjectsSlice[i])
 	}
 }
